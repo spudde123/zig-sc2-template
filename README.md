ce2b63c05699251e162895be2902be4cf9c0389d
Max Sandholm <msandhol@gmail.com>
2022-08-22 20:20:45 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Zig-sc2-template
+
+Starcraft 2 bot for competing on [sc2ai.net](https://sc2ai.net/), written in Zig 0.9.1.
+
+## Running
+
+1. Install [Zig](https://ziglang.org/).
+2. Download the template.
+3. In the folder write `zig build` and `./zig-out/bin/zig-bot.exe`.
+If you are using VS Code, you can build and launch using the configs in the .vscode folder. Debugging also works.
+4. To build a release build write `zig build -Drelease-safe`
+5. To build an executable that works on the sc2ai ladder write
+`zig build -Dtarget=x86_64-linux -Drelease-safe`. There is also a .bat file that you can modify to directly generate
+the zip for ladder. The script uses 7z from a certain path currently.
---
+++ src/main.zig
@@ -0,0 +1,58 @@
+const std = @import("std");
+
+const zig_sc2 = @import("zig-sc2");
+const bot_data = zig_sc2.bot_data;
+
+const TestBot = struct {
+
+    name: []const u8,
+    race: bot_data.Race,
+
+    pub fn onStart(
+        self: *TestBot,
+        bot: bot_data.Bot,
+        game_info: bot_data.GameInfo,
+        actions: *bot_data.Actions
+    ) void {
+        _ = self;
+        _ = game_info;
+        const enemy_start_location = game_info.enemy_start_locations[0];
+
+        for (bot.units) |unit| {
+            if (unit.unit_type == bot_data.UnitId.SCV) {
+                actions.attackPosition(unit.tag, enemy_start_location, false);
+            }
+        }
+        
+    }
+
+    pub fn onStep(
+        self: *TestBot,
+        bot: bot_data.Bot,
+        game_info: bot_data.GameInfo,
+        actions: *bot_data.Actions
+    ) void {
+        _ = game_info;
+        _ = self;
+        if (bot.game_loop > 500) actions.leaveGame();
+    }
+
+    pub fn onResult(
+        self: *TestBot,
+        bot: bot_data.Bot,
+        game_info: bot_data.GameInfo,
+        result: bot_data.Result
+    ) void {
+        _ = bot;
+        _ = game_info;
+        _ = result;
+        _ = self;
+    }
+    
+};
+
+pub fn main() !void {
+    var my_bot = TestBot{.name = "TestBot", .race = .terran};
+
+    try zig_sc2.run(&my_bot, 2, std.heap.page_allocator);
+}
