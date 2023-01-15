f2a303d02017dd1a891c42e1d6e872cf1cbc6403
Max Sandholm <msandhol@gmail.com>
2023-01-15 16:20:47 +0200

    Python script for building bots for ladder

---
+++ ladder_build.py
@@ -0,0 +1,37 @@
+import os
+import argparse
+import subprocess
+import zipfile
+
+valid_examples = {
+    "one_base_terran": True
+}
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Create a zip for the sc2ai ladder."
+    )
+    parser.add_argument("-n", "--name", required=True, help="Name of the executable in the zip. Needs to be the same as your bot name on sc2ai.net.")
+    parser.add_argument("-e", "--example", help="For example 'one_base_terran'. Leave empty when building your own bot.")
+    args = parser.parse_args()
+    if args.example and args.example not in valid_examples:
+        print(f"{args.example} is not a valid example name.")
+        return
+
+    cmd = ["zig", "build", "-Dtarget=x86_64-linux", "-Drelease-safe"]
+    exe_name = "zig-bot" if not args.example else args.example
+
+    if args.example:
+        cmd.extend(["--", args.example])
+
+    subprocess.run(cmd)
+    
+    with zipfile.ZipFile(f"{args.name}.zip", 'w', zipfile.ZIP_DEFLATED) as zip_folder:
+        files = os.listdir("./ladder_build")
+        for file in files:
+            zip_folder.write(f"./ladder_build/{file}", arcname=file)
+        zip_folder.write(f"./zig-out/bin/{exe_name}", arcname=args.name)
+
+
+if __name__ == "__main__":
+    main()
