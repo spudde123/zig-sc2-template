a4c317a1ef7989abf9c3dcd1a18b4665384d854b
Max Sandholm <msandhol@gmail.com>
2023-01-15 20:30:06 +0200

    Adjusting build script

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const fs = std.fs;
 
 pub fn build(b: *std.build.Builder) void {
     // Standard target options allows the person running `zig build` to choose
@@ -14,21 +15,28 @@ pub fn build(b: *std.build.Builder) void {
     // If build is called `zig build -- example_bot`
     // we try to build example_bot.zig in the examples folder.
     // Otherwise default to src/main.zig
-    var run_tests = true;
-    const exe = e: {
+    var main_file: []const u8 = "src/main.zig";
+    var bot_name: []const u8 = "zig-bot";
+    var buf: [256]u8 = undefined;
+
+    first_arg: {
         if (b.args) |args| {
-            var buf: [256]u8 = undefined;
-            const main_file = std.fmt.bufPrint(&buf, "examples/{s}.zig", .{args[0]}) catch {
+            const file_to_test = std.fmt.bufPrint(&buf, "examples/{s}.zig", .{args[0]}) catch {
                 std.log.err("Invalid example\n", .{});
                 return;
             };
-            run_tests = false;
-            break :e b.addExecutable(args[0], main_file);
-        } else {
-            break :e b.addExecutable("zig-bot", "src/main.zig");
+
+            const file = fs.cwd().openFile(file_to_test, .{}) catch {
+                std.log.info("Building src/main.zig\n", .{});
+                break :first_arg;
+            };
+            defer file.close();
+            main_file = file_to_test;
+            bot_name = args[0];
         }
-    };
-    
+    }
+
+    const exe = b.addExecutable(bot_name, main_file);
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.addPackagePath("zig-sc2", "lib/zig-sc2/src/runner.zig");
@@ -43,12 +51,10 @@ pub fn build(b: *std.build.Builder) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    if (run_tests) {
-        const exe_tests = b.addTest("src/main.zig");
-        exe_tests.setTarget(target);
-        exe_tests.setBuildMode(mode);
+    const exe_tests = b.addTest(main_file);
+    exe_tests.setTarget(target);
+    exe_tests.setBuildMode(mode);
 
-        const test_step = b.step("test", "Run unit tests");
-        test_step.dependOn(&exe_tests.step);
-    }
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&exe_tests.step);
 }
