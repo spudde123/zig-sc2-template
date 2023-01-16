4342e3887f51b18ab2c1b2223f67478604607460
Max Sandholm <msandhol@gmail.com>
2023-01-16 18:35:12 +0200

    Remove print

--- examples/one_base_terran.zig
+++ examples/one_base_terran.zig
@@ -547,7 +547,6 @@ const ExampleBot = struct {
             var last_scan: f32 = 0;
         };
         if (orbital_iter.next()) |orbital| {
-            std.debug.print("Visibility: {d}\n", .{bot.visibility.getValue(target)});
             const scan_needed = bot.visibility.getValue(target) != 2;
             if (scan_needed and orbital.energy >= 50 and bot.time - OrbitalScan.last_scan > 5) {
                 actions.useAbilityOnPosition(orbital.tag, .ScannerSweep_Scan, target, false);
